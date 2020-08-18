f71effc126a1e69b5158515c6b9b240c339ef7d7
admin <bigbassroller@gmail.com>
2020-08-18 03:54:57 -0700

    reading svg file

--- lib/modify_svgs.ex
+++ lib/modify_svgs.ex
@@ -8,11 +8,55 @@ defmodule ModifySvgs do
 
   ## Examples
 
-      iex> ModifySvgs.hello()
-      :world
+      iex> ModifySvgs.list_files("svg")
+      ["inbox.html.eex", ...]
 
   """
-  def hello do
-    :world
+  def list_files(path) do
+    File.ls!("#{path}")
+  end
+
+  def get_svg(path) do
+    list_files(path)
+    |> Enum.each(fn(svg) ->
+      IO.inspect("🍒 svg")
+
+      File.read!("#{path}/#{svg}")
+      |> IO.inspect
+    end)
+  end
+
+  def add_assigns_to_svg(path) do
+
+    list_files(path)
+    |> Enum.each(fn(svg) ->
+      IO.inspect("🍒 svg")
+      IO.inspect(svg)
+      {:ok, document} = Floki.parse_document(File.read!("#{path}/#{svg}"))
+      
+      html = Floki.find(document, "svg")
+      |> Enum.each(fn(html) ->
+        IO.inspect("🍒 html")
+        IO.inspect(html)
+        Floki.traverse_and_update(html, fn {"svg", attrs, children} ->
+
+          {"width", attrs, children}
+
+        end)
+      end)
+      |> IO.inspect()
+
+      # new_name = String.replace(svg, "svg", "html.eex")
+      # File.rename!("#{path}/#{svg}", "#{path}/#{new_name}")
+    end)
   end
 end
+
+
+
+
+
+
+
+
+
