f98deff82535ef747294254dfcb978bf2c8a2597
admin <bigbassroller@gmail.com>
2020-08-18 02:12:31 -0700

    initial commit

---
+++ lib/modify_svgs.ex
@@ -0,0 +1,18 @@
+defmodule ModifySvgs do
+  @moduledoc """
+  Documentation for `ModifySvgs`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ModifySvgs.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/modify_svgs_test.exs
@@ -0,0 +1,8 @@
+defmodule ModifySvgsTest do
+  use ExUnit.Case
+  doctest ModifySvgs
+
+  test "greets the world" do
+    assert ModifySvgs.hello() == :world
+  end
+end
