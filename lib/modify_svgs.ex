409a4191ac851b6477faad87107f634520ea978a
admin <bigbassroller@gmail.com>
2020-08-19 03:20:11 -0700

    clean up and added bootstrap icons

--- lib/modify_svgs.ex
+++ lib/modify_svgs.ex
@@ -4,94 +4,33 @@ defmodule ModifySvgs do
   """
 
   @doc """
-  list_files/1
+  convert_svg_to_eex/3
 
   ## Examples
 
-      iex> ModifySvgs.list_files("svg")
-      ["inbox.html.eex", ...]
+      iex> ModifySvgs.convert_svg_to_eex("icons", "../my_app/lib/my_app_web/templates/icons", "<%= assigns[:svg_class] %>")
+      "<svg class="<%= assigns[:svg_class] %>..." ...some long svg stuff</svg>", "<svg class="<%= assigns[:svg_class] %>..." ...some long svg stuff</svg>"
 
   """
-  def list_files(path) do
-    File.ls!("#{path}")
-  end
-
-  @doc """
-  get_svg/1
-
-  ## Examples
-
-      iex> ModifySvgs.get_svg("svg")
-      "<svg ...some long svg stuff</svg>", "<svg ...some long svg stuff</svg>"
-
-  """
-  def add_assigns_to_svgs(path) do
-    list_files(path)
+  def convert_svg_to_eex(source, dest, attrs) do
+    File.ls!("#{source}")
     |> Enum.each(fn(svg) ->
-      {:ok, contents} = File.read!("#{path}/#{svg}")
+      {:ok, contents} = File.read!("#{source}/#{svg}")
       |> Floki.parse_document
       
       attributes = Floki.attribute(contents, "class")
-      added_attribute = ~S("<%= assigns[:svg_class] %>")
 
-      IO.inspect("🍒 added_attribute")
-      IO.inspect(added_attribute)
-
-      # IO.inspect("🍒 updates")
-      # updates = Floki.attr(contents, "svg", "class", fn _ -> "#{added_attribute} #{attributes}" end)
       updates = Floki.attr(contents, "svg", "class", fn _ -> "assigns #{attributes}" end)
-      |> IO.inspect
-      |> Floki.raw_html(encode: false)
-      |> String.replace("assigns", ~S(<%= assigns[:svg_class] %>))
-     
-      IO.inspect("🍒 updates")
-      IO.inspect(updates)
+      |> Floki.raw_html
+      |> String.replace("assigns", attrs)
 
       new_name = String.replace(svg, "svg", "html.eex")
-   
-      IO.inspect("🍒 new_name")
-      IO.inspect(new_name)
+      mkdir = File.mkdir_p("#{dest}")
 
-
-      new_file = File.write("icon-templates/#{new_name}", updates)
-      
-      # File.rename!("#{path}/#{svg}", "#{path}/#{new_name}")
+      new_file = File.write("#{dest}/_#{new_name}", updates) 
     end)
   end
 
-  def insert_attrs(path) do
-    IO.inspect("data 🍒")
-    # {:ok, svg} = get_svg(path)
-    data = get_svg(path)
-    |> IO.inspect()
-
-  end
-
-
-  # def add_assigns_to_svg(path) do
-
-  #   list_files(path)
-  #   |> Enum.each(fn(svg) ->
-  #     IO.inspect("🍒 svg")
-  #     IO.inspect(svg)
-  #     {:ok, document} = Floki.parse_document(File.read!("#{path}/#{svg}"))
-      
-  #     html = Floki.find(document, "svg")
-  #     |> Enum.each(fn(html) ->
-  #       IO.inspect("🍒 html")
-  #       IO.inspect(html)
-  #       Floki.traverse_and_update(html, fn {"svg", attrs, children} ->
-
-  #         {"width", attrs, children}
-
-  #       end)
-  #     end)
-  #     |> IO.inspect()
-
-  #     # new_name = String.replace(svg, "svg", "html.eex")
-  #     # File.rename!("#{path}/#{svg}", "#{path}/#{new_name}")
-  #   end)
-  # end
 
 end
 
